2514e3b62799b1da8deb3527bbc3ec9386a9556b
alfaisal7313 <alfaisal7313@gmain.com>
2024-05-08 17:17:40 +0700

    module_2: Add header component

---
+++ component/header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full border-b border-slate-200 py-6">
+        <div class="container">
+            <div class="flex justify-beetween item-center">
+                <div>
+
+                </div>
+                <nav class="flex item-center gap-6">
+
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
