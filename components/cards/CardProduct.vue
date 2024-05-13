1d16b2a77cc2876129dfcd41c61d71f7b58cfc74
faisal7313 <fais.part3@gmail.com>
2024-05-13 14:12:53 +0700

    module_3: [fix] pointer style

--- components/cards/CardProduct.vue
+++ components/cards/CardProduct.vue
@@ -23,7 +23,7 @@ const props = defineProps({
         <span class="text-sm font-normal">{{ props.product.price }}</span>
       </div>
       <div
-        class="`cursor-pointer` absolute -top-5 right-7 w-[50px] h-[50px] bg-white shadow-xl rounded-full flex justify-center items-center hover:bg-blue-600 hover:text-white transition duration-300"
+        class="cursor-pointer absolute -top-5 right-7 w-[50px] h-[50px] bg-white shadow-xl rounded-full flex justify-center items-center hover:bg-blue-600 hover:text-white transition duration-300"
       >
         <i class="ri-shopping-cart-2-line"></i>
       </div>
